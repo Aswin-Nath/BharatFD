8438647d007ed2a9b34a96f1607538caf7c9926c
Aswin Nath T.E <aswinnathte125@gmail.com>
2025-02-03 00:05:22 +0530

    Update AdminPanel.js

--- src/Component/Admin/AdminHome/AdminPanel.js
+++ src/Component/Admin/AdminHome/AdminPanel.js
@@ -5,8 +5,9 @@ import "./AdminPanel.css";
 function AdminPanel() {
   return (
     <div className="admin-panel-container">
-      <h2>Admin Panel</h2>
+
       <div className="nav-links">
+      <h2>Admin Panel</h2>
         <Link to="/admin/queries" className="unanswered">Queries</Link>
         <Link to="/admin/edit-faq" className="edit-faq">Edit FAQ</Link>
         <Link to="/admin/dummy-user" className="dummy-user">Dummy User</Link>
